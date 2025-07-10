27ea45109b7341d6abf9dcbeb91fe097bd95311a
Maki <Maiiiiiiiho@gmail.com>
2025-07-10 23:04:49 +0800

    day1

---
+++ c2variable/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main() {
+    
+    int x; //declaration
+    x = 5;
+
+    int y = 10;
+
+    std::cout << x << std::endl;
+    std::cout << y << std::endl;
+
+    int days = 7.5;
+    std::cout << days << std::endl;
+
+    double price = 1.99;
+    double gpa = 2.6;
+
+    std::cout << price << std::endl;
+
+
+    char grade = 'A';
+    char initial = 'BC'; //warning
+    char currency = '$';
+
+    std::cout << initial << std::endl;
+
+
+    // boolean (true or false)
+    bool student = true;
+    bool power = true;
+    bool forSale = true;
+
+    //string
+    std::string name = "Maki";
+    std::string day = "Friday";
+    std::string food = "pizza";
+    std::string address = "123 Fake Province";
+
+    std::cout << "Hello ";
+    std::cout << day << std::endl;
+    
+
+
+
+    return 0;
+}
---
+++ c3Const/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    const double PI = 3.1415926;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << "cm" << std::endl;
+
+    const int LIGHT_SPEED = 299792458;
+    
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+}
