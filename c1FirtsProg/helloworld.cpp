542231d3e34e551a3fba5bac58ebc3c3489bbfe1
Maki <Maiiiiiiiho@gmail.com>
2025-07-10 21:35:22 +0800

    init repo

---
+++ c1FirtsProg/helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+    // This is a comment
+    /*
+    This
+    is
+    also
+    a
+    comment
+    */
+
+    std::cout << "i like pizza!" << std::endl; //flesh the output buffer
+    std::cout << "It's really good!"<< "\n";
+    return 0;
+}
